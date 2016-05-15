2054dd366d137163eae646defb14d24bc166ce88
Jakub Zelenka <bukka@php.net>
2016-05-15 15:43:59 +0100

    Fix PHPC_HASH_APPLY_PROTECTION in 5

--- phpc.h
+++ phpc.h
@@ -358,10 +358,10 @@ typedef long phpc_res_value_t;
 /* HASH */
 
 /* apply count */
-#define PHPC_HASH_GET_APPLY_COUNT(_ht) _ht->nApplyCount
-#define PHPC_HASH_INC_APPLY_COUNT(_ht) _ht->nApplyCount++
-#define PHPC_HASH_DEC_APPLY_COUNT(_ht) _ht->nApplyCount--
-#define PHPC_HASH_APPLY_PROTECTION     1
+#define PHPC_HASH_GET_APPLY_COUNT(_ht)  _ht->nApplyCount
+#define PHPC_HASH_INC_APPLY_COUNT(_ht)  _ht->nApplyCount++
+#define PHPC_HASH_DEC_APPLY_COUNT(_ht)  _ht->nApplyCount--
+#define PHPC_HASH_APPLY_PROTECTION(_ht) 1
 
 /* update */
 #define PHPC_HASH_INDEX_UPDATE(_ht, _idx, _pzv) \
